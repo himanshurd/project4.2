872e80f8aee3bb2dacfc0dd8cc9697b577322e24
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-10-16 22:40:05 -0700

    Add files via upload

---
+++ project4.2/wordclient.py
@@ -0,0 +1,81 @@
+import sys
+import socket
+
+# How many bytes is the word length?
+WORD_LEN_SIZE = 2
+
+def usage():
+    print("usage: wordclient.py server port", file=sys.stderr)
+
+packet_buffer = b''
+
+def get_next_word_packet(s):
+    """
+    Return the next word packet from the stream.
+
+    The word packet consists of the encoded word length followed by the
+    UTF-8-encoded word.
+
+    Returns None if there are no more words, i.e. the server has hung
+    up.
+    """
+
+    global packet_buffer
+
+    # TODO -- Write me!
+    packet_buffer += s.recv(5)
+    byte_to_int = (int.from_bytes((packet_buffer[:2]), "big"))
+    extend_string = byte_to_int + 2
+
+    while True:
+        if packet_buffer != b'':
+            while len(packet_buffer) < extend_string:
+                packet_buffer
+            word_packet = packet_buffer[:extend_string]
+            packet_buffer = packet_buffer[extend_string:]
+            return word_packet
+        else:
+            return None
+
+def extract_word(word_packet):
+    """
+    Extract a word from a word packet.
+
+    word_packet: a word packet consisting of the encoded word length
+    followed by the UTF-8 word.
+
+    Returns the word decoded as a string.
+    """
+
+    # TODO -- Write me!
+    return word_packet[2:].decode()
+
+# Do not modify:
+
+def main(argv):
+    try:
+        host = argv[1]
+        port = int(argv[2])
+    except:
+        usage()
+        return 1
+
+    s = socket.socket()
+    s.connect((host, port))
+
+    print("Getting words:")
+
+    while True:
+        word_packet = get_next_word_packet(s)
+
+        if word_packet is None:
+            break
+
+        word = extract_word(word_packet)
+
+        print(f"    {word}")
+
+    s.close()
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
